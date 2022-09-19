16f3300032da523685fcaf5894d74c24723c522b
Hanif <hanif.hakim@spesolution.com>
2022-09-19 21:15:40 +0700

    Integrate with course service part 2 [courses]

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('courses');
-});
+const coursesHandler = require("./handler/courses");
+
+router.get("/", coursesHandler.getAll);
+router.get("/:id", coursesHandler.get);
+router.post("/", coursesHandler.create);
+router.put("/:id", coursesHandler.update);
+router.delete("/:id", coursesHandler.destroy);
 
 module.exports = router;
