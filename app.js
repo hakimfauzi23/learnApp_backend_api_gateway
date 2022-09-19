ff5467840d6cd8e64500453d7287679fd4aab78b
Hanif <hanif.hakim@spesolution.com>
2022-09-19 12:06:13 +0700

    Integrate with course service part 1 [mentors]

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const mediaRouter = require("./routes/media");
 const ordersRouter = require("./routes/orders");
 const paymentsRouter = require("./routes/payments");
 const refreshTokensRouter = require("./routes/refreshTokens");
+const mentorsRouter = require('./routes/mentors')
 
 const verifyToken = require("./middlewares/verifyToken");
 const app = express();
@@ -28,5 +29,6 @@ app.use("/media", mediaRouter);
 app.use("/orders", ordersRouter);
 app.use("/payments", paymentsRouter);
 app.use("/refresh-tokens", refreshTokensRouter);
+app.use("/mentors", mentorsRouter);
 
 module.exports = app;
